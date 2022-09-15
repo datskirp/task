36e985af7a09d121c01abbccb79f5276332f39ff
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 15:25:31 +0300

    Task7 committed

--- Task7.php
+++ Task7.php
@@ -2,10 +2,16 @@
 
 namespace src;
 
-class Task
+class Task7
 {
-    public function main():
+    public function main(array $arr, int $position): array
     {
-        
+        if (empty($arr) or count($arr) < 2) {
+            throw new \InvalidArgumentException('Method main only accepts arrays with 2 or more elements.');
+        }
+
+        unset($arr[$position]);
+
+        return [...$arr];
     }
 }
