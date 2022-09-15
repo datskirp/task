329808c0b99c361f03c1ecb59d94c41fde4aafa0
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 13:00:15 +0300

    commited Task3

--- Task3.php
+++ Task3.php
@@ -2,10 +2,24 @@
 
 namespace src;
 
-class Task
+class Task3
 {
-    public function main():
+    public function main(int $num): int
     {
+        //validate if input integer > 0 and at least 2 digits long
+        if ($num < 10) {
+            throw new \InvalidArgumentException('Method main only accepts integers >= 10. Input was: '.$num);
+        }
+        //splitting input number into an array
+        $arr = str_split((string)$num);
+        //Looping until there's only one digit left
+        while (count($arr) > 1) {
+            //Changing data type of the array to integer
+            $arr = array_map('intval', $arr);
+            //calculating the sum of the $arr array and feeding it to str_split function
+            $arr = str_split((string)array_sum($arr));
+        }
 
+        return (int) $arr[0];
     }
 }
