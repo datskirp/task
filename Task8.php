5a227ebbb68ee61038c4c7af67fcabc49b60be31
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 15:56:33 +0300

    Task8 comitted

--- Task8.php
+++ Task8.php
@@ -2,10 +2,21 @@
 
 namespace src;
 
-class Task
+class Task8
 {
-    public function main():
+    public function main(string $json): string
     {
-        
+        $result = json_decode($json, true);
+        if ($result == null) {
+            throw new \InvalidArgumentException('Method main accepts only valid json format.');
+        }
+        $str = '';
+        //creating an anonymous function to hold information in a string
+        $func = function ($item, $key) use (&$str) {
+            $str .= $key . ': ' . $item . "\r\n";
+        };
+        array_walk_recursive($result, $func);
+
+        return $str;
     }
 }
