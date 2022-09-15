e1a00647507007fd95256c09be8522838c6973e8
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 15:14:09 +0300

    forgot csFixer check

--- Task4.php
+++ Task4.php
@@ -6,11 +6,13 @@ class Task4
 {
     public function main(string $input): string
     {
-        if(empty($input))
+        if (empty($input)) {
             throw new \InvalidArgumentException('Method main only accepts non empty strings. Input was: '.$input);
+        }
         $input = strtolower($input);
-        $needles = ["_", "-"];
-        $input = str_replace($needles, " ", $input);
-        return str_replace(" ", "", ucwords($input));
+        $needles = ['_', '-'];
+        $input = str_replace($needles, ' ', $input);
+
+        return str_replace(' ', '', ucwords($input));
     }
 }
