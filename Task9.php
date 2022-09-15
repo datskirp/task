f94639950ce928c4f9a02cf57f68cdcf70694a2c
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 17:18:48 +0300

    Task9 comitted

--- Task9.php
+++ Task9.php
@@ -2,10 +2,27 @@
 
 namespace src;
 
-class Task
+class Task9
 {
-    public function main():
+    public function main(array $arr, int $number): array
     {
-        
+        if (empty($arr) or count($arr) < 3) {
+            throw new \InvalidArgumentException('Input array must not be empty or have less than 3 elements');
+        }
+        $result = [];
+        $answer = [];
+        for ($i = 0; $i < count($arr) - 2; $i++) {
+            if (array_sum(array_slice($arr, $i, 3)) == $number) {
+                $result[] = $i;
+            } else {
+                continue;
+            }
+        }
+        // Concatenating answer statement from input array
+        foreach ($result as $item) {
+            $answer[] = $arr[$item] . ' + ' . $arr[$item + 1] . ' + ' . $arr[$item + 2] . ' = ' . $number;
+        }
+
+        return $answer;
     }
 }
