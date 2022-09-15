dc363b147071982fb4f2f21a99b076f05668d8e4
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-15 17:34:48 +0300

    Task10 committed

--- Task10.php
+++ Task10.php
@@ -2,10 +2,22 @@
 
 namespace src;
 
-class Task
+class Task10
 {
-    public function main():
+    public function main(int $input): array
     {
-        
+        if ($input <= 0) {
+            throw new \InvalidArgumentException('Method main only accepts positive integers. Input was: '.$input);
+        }
+        $result = [];
+        while ($input > 1) {
+            $result[] = $input;
+            //checking for odd or even can also be done through
+            //modulus operator %
+            $input = $input & 1 ? $input * 3 + 1 : $input / 2;
+        }
+        $result[] = 1;
+
+        return $result;
     }
 }
