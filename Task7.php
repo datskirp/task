5538263bc75ea9ca0420e1adcca5715aa5bebca2
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-16 12:17:09 +0300

    mistakes fixed 1

--- Task7.php
+++ Task7.php
@@ -6,12 +6,23 @@ class Task7
 {
     public function main(array $arr, int $position): array
     {
-        if (empty($arr) or count($arr) < 2) {
-            throw new \InvalidArgumentException('Method main only accepts arrays with 2 or more elements.');
+        if (!$this->validateInput($arr, $position)) {
+            throw new \InvalidArgumentException('Invalid input');
         }
 
         unset($arr[$position]);
 
         return [...$arr];
+    }
+
+    private function validateInput(array $arr, int $position): bool
+    {
+        if (empty($arr) or count($arr) < 2) {
+            return false;
+        } elseif ($position < 0 or count($arr) <= $position) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
