034febbe45d39307cd4d2e50607e37837d53b04d
Aliaksandr <alex.kirpichonak@gmail.com>
2022-09-18 15:20:26 +0300

    Task12 committed

--- Task12.php
+++ Task12.php
@@ -2,10 +2,76 @@
 
 namespace src;
 
-class Task
+class Task12
 {
-    public function main():
+    private int $firstNumber;
+    private int $secondNumber;
+    private int $result;
+
+    public function __construct(int $firstNumber, int $secondNumber)
     {
-        
+        $this->firstNumber = $firstNumber;
+        $this->secondNumber = $secondNumber;
+    }
+
+    public function add()
+    {
+        $this->result = $this->firstNumber + $this->secondNumber;
+
+        return $this;
+    }
+
+    public function subtract()
+    {
+        $this->result = $this->firstNumber - $this->secondNumber;
+
+        return $this;
+    }
+
+    public function multiply()
+    {
+        $this->result = $this->firstNumber * $this->secondNumber;
+
+        return $this;
+    }
+
+    public function divide()
+    {
+        $this->result = $this->firstNumber / $this->secondNumber;
+
+        return $this;
+    }
+
+    public function addBy(int $number)
+    {
+        $this->result += $number;
+
+        return $this;
+    }
+
+    public function subtractBy(int $number)
+    {
+        $this->result -= $number;
+
+        return $this;
+    }
+
+    public function divideBy(int $number)
+    {
+        $this->result /= $number;
+
+        return $this;
+    }
+
+    public function multiplyBy(int $number)
+    {
+        $this->result *= $number;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return strval($this->result);
     }
 }
